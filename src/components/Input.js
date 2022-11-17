f0b5b6b18f7815890877a0daaacc8a16cb2c243d
Crownkay <adekomsk@gmail.com>
2022-11-17 09:13:04 -0700

    first commit

---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Input() {
+  return (
+    <section >
+      <form className="form">
+        <input className="input--text" type="text" placeholder="shut up" />
+        <input className="input--text" type="text" placeholder="take the money" />
+      <button className="input--button">Get a new meme image 🖼 </button>
+      </form>
+    </section>
+  );
+}
+
+export default Input;
