a6c4a7a715102dde19eb54b3e225b9e65f15070d
Crownkay <adekomsk@gmail.com>
2022-11-22 09:37:20 -0700

    setState

--- src/components/State.js
+++ src/components/State.js
@@ -1,38 +1,19 @@
 import React, {useState} from "react"
 
 function State(){
- const [count, setCount] = useState(0)
-  function handlePlus(){
-    setCount( prevCount => prevCount + 1) 
+  const [thingsArray, SetThingsArray] = useState(["Thing 1", "Thing 2"])
+  function addItem (){
+    SetThingsArray(prevThingsArray => {
+      return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`]
+    })
   }
-  function handleMinus(){
-    setCount( prevCount => prevCount - 1) 
-  }
-  function handleReset(){
-    setCount("0") 
-  }
-
-  const isGoingOut = true
-  let date = new Date()
-  let hour = date.toLocaleTimeString();
-  let time = date.getHours()
-  let today = date.toDateString()
-
-  let answer 
-
-  answer = time < 12 ? "Yes" : "No"
-
+  const thingsElems = thingsArray.map(item => <p key = {item}>{item}</p>)
   return (
-    <div className="state">
-      <h2>Welcome today is {today}</h2>
-      <h3>Do you feel like going out</h3>
-      <p>{answer} the time is {hour}</p>
-      <h3>{count}</h3>
-      <button onClick={handlePlus}>+</button>
-      <button onClick={handleMinus}>-</button>
-      <button onClick={handleReset}>reset</button>
+    <div>
+      <button onClick={addItem}>Add item</button>
+      {thingsElems}
     </div>
   )
 }
 
-export default State;
+export default State
